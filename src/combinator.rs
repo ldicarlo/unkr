db403f4503084530566eacf5b3044d5ef667101b
Luca Di Carlo <luca@di-carlo.fr>
2022-09-11 19:04:40 +0200

    wip

--- src/combinator.rs
+++ src/combinator.rs
@@ -17,7 +17,6 @@ use std::collections::HashMap;
 /// - B,C,A
 /// - C,B,A
 /// - C,A,B
-///
 
 type Combination = Vec<Vec<Step>>;
 
@@ -29,7 +28,13 @@ struct Step {
 pub fn get_combinations(vector: Vec<&str>, range: Vec<u8>) {}
 
 pub fn combinate_strings(vector: Vec<&str>) -> Vec<Vec<String>> {
-    vec![]
+    let mut result: Vec<Vec<String>> = Vec::new();
+    for one in vector.iter() {
+        let mut array: Vec<Vec<String>> = Vec::new();
+        let others: Vec<&str> = vector.clone().iter().filter(|v| *v == one).collect();
+    }
+
+    result
 }
 
 #[cfg(test)]
