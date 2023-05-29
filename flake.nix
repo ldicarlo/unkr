b2b574d91465ad28d0f03c097ebe91fed57d37d7
Luca Di Carlo <luca@di-carlo.fr>
2023-05-29 21:18:44 +0200

    wip

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "UNKR";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
