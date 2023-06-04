ec2b690ad2bd1168a11dde172660a0b3ad3e52c8
Luca Di Carlo <luca@di-carlo.fr>
2023-06-04 13:50:00 +0200

    wip

--- src/base.rs
+++ src/base.rs
@@ -29,6 +29,39 @@ pub fn from_digits(input: Vec<u8>, base: usize) -> u64 {
     n
 }
 
+fn increment_with_bases(input: Vec<u8>, bases: Vec<usize>) -> Vec<u8> {
+    let mut number = from_digits_and_bases(input, bases.clone());
+
+    let mut result = to_digits_and_bases(number + 1, bases.clone());
+    while result.clone().into_iter().any(|a| a == 0) {
+        number = number + 1;
+        result = to_digits_and_bases(number + 1, bases.clone());
+    }
+
+    result
+}
+
+fn from_digits_and_bases(input: Vec<u8>, bases: Vec<usize>) -> u64 {
+    let mut n = 0;
+    let base_len: isize = bases.len() as isize;
+    input.into_iter().enumerate().for_each(|(i, d)| {
+        n = n * bases[((base_len - i as isize - 1) % base_len) as usize] as u64 + d as u64
+    });
+    n
+}
+
+fn to_digits_and_bases(mut input: u64, bases: Vec<usize>) -> Vec<u8> {
+    let mut result = Vec::new();
+    let base_len: isize = bases.len() as isize;
+    let mut i: usize = 0;
+    while input > 0 {
+        result.insert(0, (input % bases[((base_len - i as isize - 1) % base_len) as usize] as u64) as u8);
+        input = input / bases[((base_len - i as isize - 1) % base_len) as usize] as u64;
+        i = (i + 1) % bases.len();
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -37,5 +70,31 @@ mod tests {
         assert_eq!(super::from_digits(vec![1, 0], 2), 2);
         assert_eq!(super::to_digits(2, 2), vec![1, 0]);
         assert_eq!(super::increment(vec![23, 24, 25], 26), vec![23, 25, 1]);
+        assert_eq!(super::increment(vec![23, 25, 25], 26), vec![24, 1, 1]);
+    }
+
+    #[test]
+    fn it_works_2() {
+        assert_eq!(
+            super::increment_with_bases(vec![23, 24, 25], vec![26]),
+            vec![23, 25, 1]
+        );
+        assert_eq!(
+            super::increment_with_bases(vec![23, 25, 25], vec![26]),
+            vec![24, 1, 1]
+        );
+    }
+
+    #[test]
+    fn multiple_bases_works() {
+        assert_eq!(
+            super::increment_with_bases(
+                // B, C, Z
+                vec![2, 3, 26],
+                vec![27, 4, 27]
+            ),
+            // C,A,A
+            vec![3, 1, 1]
+        );
     }
 }
