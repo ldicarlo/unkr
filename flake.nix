d58bb6433e4de3c17868f270b683d8501dbf47fd
Luca Di Carlo <luca@di-carlo.fr>
2023-09-30 21:00:06 +0200

    wip

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        lib = import lib { };
+        craneLib = crane.lib.${system};
+        code = pkgs.callPackage ./. { inherit nixpkgs system; };
+      in
+      rec {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.cargo
+          ];
+        };
+
+        packages = rec {
+          default = unkr;
+          # https://crane.dev/getting-started.html
+          unkr = craneLib.buildPackage rec {
+            name = "unkr";
+            src = craneLib.cleanCargoSource ./.;
+            rust-dependencies = craneLib.buildDepsOnly {
+              inherit src;
+            };
+
+            rust-package-binary = craneLib.buildPackage {
+              inherit src;
+              cargoArtifacts = rust-dependencies;
+
+              doCheck = false;
+            };
+            # postUnpack = ''
+            #   cd $sourceRoot/api
+            #   sourceRoot="."
+            # '';
+          };
+        };
+        #    defaultPackage = packages.all;
+      });
+}
