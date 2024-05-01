2610aeea440ed03c5fef2a63db0461b41d13b174
Luca Di Carlo <luca@dicarlo.email>
2024-05-01 15:42:54 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
               (craneLib.filterCargoSources path type)
             ;
           };
-          buildInputs = with pkgs; [ pkg-config openssl rustc ];
+          buildInputs = with pkgs; [ pkg-config openssl ];
         };
         cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
           pname = "unkr";
