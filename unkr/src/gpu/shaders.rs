0a54d13ec07f68416cd229c5bc6e95fd9aebfab8
Luca Di Carlo <luca@dicarlo.email>
2026-01-12 13:43:51 +0100

    wip

--- unkr/src/gpu/shaders.rs
+++ unkr/src/gpu/shaders.rs
@@ -13,3 +13,11 @@ pub mod fs {
         bytes: "image_shader-image_fs.spv",
     }
 }
+
+#[cfg(not(feature = "use-glsl-shader"))]
+pub mod cs {
+    vulkano_shaders::shader! {
+        root_path_env: "SHADER_OUT_DIR",
+        bytes: "image_shader-add_cs.spv",
+    }
+}
